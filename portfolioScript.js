876ce6246ad01e7e330f2cd2ea7e76629c37a444
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-08-05 11:11:31 -0500

    recipe links

--- portfolioScript.js
+++ portfolioScript.js
@@ -82,8 +82,8 @@ const object = [
     imgMain: 'images/recipe-l.png',
     technologies: ['Ruby on Rails', 'Ruby', 'Heroku', 'RSpec', 'Capybara'],
     techMain: ['Ruby on Rails', 'Ruby', 'Heroku', 'RSpec'],
-    demo: 'https://math-magicians-jsotomayor.netlify.app/',
-    source: 'https://github.com/Juanse7793/Math-Magicians',
+    demo: 'https://recipe-app-juanse7793.herokuapp.com/',
+    source: 'https://github.com/Juanse7793/Recipe-app',
     description: 'The Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.',
   },
 ];
